6187eca06f016f29f223ad0b2e94602876e994aa
Minjae Lee <dlalswotl@naver.com>
2019-10-09 22:34:11 +0900

    수정

--- app/src/main/java/com/example/rock/MainActivity.java
+++ app/src/main/java/com/example/rock/MainActivity.java
@@ -5,11 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     EditText editChoice;
     TextView tv_start;
+    ImageView rock,scissor,paper;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tv_start=(TextView)findViewById(R.id.textView2);
         tv_start.setOnClickListener(this);
 
+        rock=(ImageView)findViewById(R.id.rock);
+        scissor=(ImageView)findViewById(R.id.Scissor);
+        paper=(ImageView)findViewById(R.id.paper);
+
+        rock.setOnClickListener(this);
+        scissor.setOnClickListener(this);
+        paper.setOnClickListener(this);
+
 
     }
 
@@ -27,12 +39,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if(v.getId()==R.id.textView2){
             String userChoice;int choice;
             userChoice=editChoice.getText().toString();
-            choice=Integer.parseInt(userChoice);
-            if(choice>0&&choice<4) {
-                Intent intent = new Intent(this, SecondActivity.class);
-                intent.putExtra("choice",choice);
-                startActivity(intent);
+
+            if(userChoice.length()>0) {
+                choice=Integer.parseInt(userChoice);
+                if (choice > 0 && choice <4) {
+                    Intent intent = new Intent(this, SecondActivity.class);
+                    intent.putExtra("choice", choice);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(this, "1~3사이의 숫자를 입력해주세요", Toast.LENGTH_SHORT).show();
+                }
             }
+            else
+                Toast.makeText(this, "가위(1) 바위(2) 보(3)를 선택해 주세요", Toast.LENGTH_SHORT).show();
         }
+        else if(v.getId()==R.id.Scissor){
+            editChoice.setText("1");
+            Toast.makeText(this, "가위를 선택하셨습니다", Toast.LENGTH_SHORT).show();
+        }
+
+        else if(v.getId()==R.id.rock){
+            editChoice.setText("2");
+            Toast.makeText(this, "바위를 선택하셨습니다", Toast.LENGTH_SHORT).show();
+        }
+
+        else if(v.getId()==R.id.paper){
+            editChoice.setText("3");
+            Toast.makeText(this, "보를 선택하셨습니다", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
--- app/src/main/java/com/example/rock/SecondActivity.java
+++ app/src/main/java/com/example/rock/SecondActivity.java
@@ -1,22 +1,98 @@
 package com.example.rock;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
     ImageView playerImg;
     ImageView comImg;
+    TextView resultText;
+    Button regameBtn;
+    int userChoice,comChoice;
+    String result;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        comChoice = (int) (1+3*Math.random());
+        Log.d("random", "onCreate"+comChoice);
+        userChoice= getIntent().getIntExtra("choice",0);
+        playerImg=(ImageView)findViewById(R.id.userImg);
+        comImg=(ImageView)findViewById(R.id.comImg);
+        resultText=(TextView)findViewById(R.id.ResultText);
+        regameBtn=(Button)findViewById(R.id.RegameBtn);
+
+        setImage(playerImg,userChoice);
+        setImage(comImg,comChoice);
+
+
+        switch(userChoice){
+            case 1:
+                if(comChoice==1)
+                    result="비겼습니다";
+                else if(comChoice==2){
+                    result="당신의 패배입니다";
+                }
+                else if(comChoice==3){
+                    result="당신의 승리입니다";
+                }break;
+
+            case 2:
+                if(comChoice==1)
+                    result="당신의 승리입니다";
+                else if(comChoice==2){
+                    result="비겼습니다";
+                }
+                else if(comChoice==3) {
+                    result = "당신의 패배입니다";
+                }break;
+
+            case 3:
+                if(comChoice==1)
+                    result="당신의 패배입니다";
+                else if(comChoice==2){
+                    result="당신의 승리입니다.";
+                }
+                else if(comChoice==3) {
+                    result = "비겼습니다";
+                }break;
+
+        }
+
+        resultText.setText(result);
+        regameBtn.setOnClickListener(this);
+    }
+    public void setImage(ImageView v,int choice){
+        switch(choice){
+            case 1:
+                v.setImageResource(R.drawable.math_img_1);
+                break;
+            case 2:
+                v.setImageResource(R.drawable.math_img_2);
+                break;
+            case 3:
+                v.setImageResource(R.drawable.math_img_3);
+                break;
+        }
     }
 
     @Override
     public void onClick(View v) {
+        if(v.getId()==R.id.RegameBtn){
+            finish();
+           /* Intent i=new Intent(this,MainActivity.class);
+            startActivity(i);*/
+        }
 
     }
 }
